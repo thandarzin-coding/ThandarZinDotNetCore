3db93a5b39f9e99110800879278a2528f1dd0622
Thandar Zin <thandarzin@acedatasystems.com>
2024-01-09 23:04:12 +0630

    [Complete]EfCoreExampleCRUD

--- ThandarZinDotNetCore.ConsoleApp/Program.cs
+++ ThandarZinDotNetCore.ConsoleApp/Program.cs
@@ -1,13 +1,18 @@
 ﻿using ThandarZinDotNetCore.ConsoleApp.AdoDotNetExample;
 using ThandarZinDotNetCore.ConsoleApp.DapperExamples;
+using ThandarZinDotNetCore.ConsoleApp.EfCoreExamples;
 
 Console.WriteLine("Hello, World!");
 
 //AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
 //adoDotNetExample.Run();
 
-DapperExample dapperExample = new DapperExample();
-dapperExample.Run();
+//DapperExample dapperExample = new DapperExample();
+//dapperExample.Run
+
+EfCoreExample efCoreExample = new EfCoreExample();
+efCoreExample.Run();
+
 
 Console.ReadKey();
 
